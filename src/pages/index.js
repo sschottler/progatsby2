0a7a19d275e113bcb68bedadbf96d071a8c534e3
Scott Schottler <scottschottler@gmail.com>
2019-01-01 13:07:08 -0500

    lowercase listing to fix netlify build error

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Listing from '../components/Listing'
+import Listing from '../components/listing'
 
 const IndexPage = ({ location }) => (
   <Layout location={location}>
